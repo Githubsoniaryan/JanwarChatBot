21cca5e15ee7a4edb43573f01ddb23252933695f
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2024-01-28 13:15:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "25667600"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "80dce690dfd8a96f83f20bbe23c28258"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = getenv("OWNER_ID", "1808943146")
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+OWNER_ID = getenv("OWNER_ID", "1777910193")
+SUPPORT_GRP = "royalgroupop"
+UPDATE_CHNL = "itsRareBeautySelenaGomez"
+OWNER_USERNAME = "AnimalJanwarrrr"
 
